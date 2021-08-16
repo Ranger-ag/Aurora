17d088cb3f229b9da0c362a47c80a60c581ba372
RedFox14-osu <76632056+RedFox14-osu@users.noreply.github.com>
2021-08-16 14:28:19 +0300

    1.0

--- Assets/Project Files/Scripts/MainPlayerControler.cs
+++ Assets/Project Files/Scripts/MainPlayerControler.cs
@@ -11,7 +11,7 @@ public class MainPlayerControler : MonoBehaviour
     [SerializeField] private float m_BallHorizontalSpeed;
 
     private Rigidbody m_Rigidbody;
-        
+
     #endregion
 
     #region UnityCallbacks
@@ -21,10 +21,28 @@ public class MainPlayerControler : MonoBehaviour
         m_Rigidbody = GetComponent<Rigidbody>();
     }
 
+    private Vector3 GetWorldPos()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            return hit.point;
+        }
+        return Vector3.zero;
+    }
+
     private void Update()
     {
         float HInput = Input.GetAxis("Horizontal");
-        m_Rigidbody.AddForce(new Vector3(HInput*m_BallHorizontalSpeed, 0f, 1f) * m_BallSpeed);
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.nearClipPlane;
+        mousePos = GetWorldPos();
+        if (mousePos != Vector3.zero)
+        {
+            transform.position = new Vector3(mousePos.x, transform.position.y, transform.position.z);
+            Vector3 forceVector = mousePos - transform.position;
+            m_Rigidbody.AddForce(new Vector3(forceVector.x * m_BallHorizontalSpeed, 0, m_BallSpeed));
+        }
     }
     #endregion
 }
